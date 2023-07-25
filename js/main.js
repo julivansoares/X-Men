ef380f7491c00a2191d5bb42b2fcb706ad115f18
julivansoares <julivan.soares@outlook.com>
2023-07-25 19:49:11 -0300

    finalização do script

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ const pesonagens=[
         nome:"Lince Negra",
         card:"/img/card-lince-negra.jpg",
         poder:"Uma mutante, Pryde possui uma habilidade faseada que lhe permite, assim como objetos ou pessoas com quem ela está em contato, tornar-se intangível. ",
-        pesonagem:"/img/card-lince-negra.png",
+        pesonagen:"/img/card-lince-negra.png"
     
     },
     {
@@ -30,7 +30,7 @@ const pesonagens=[
         nome:"Magneto",
         card:"/img/card-magneto.jpg",
         poder:"Magneto foi recentemente reclassificado como um mutante de nível ômega, seus poderes são incrivelmente vastos. Ele pode controlar e manipular campos magnéticos que vão muito além da habilidade de controlar metal.",
-        pesonagem:"/img/card-magneto.png"
+        pesonagen:"/img/card-magneto.png"
     
     },
     {
@@ -38,7 +38,7 @@ const pesonagens=[
         nome:"Noturno",
         card:"/img/card-noturno.jpg",
         poder:"Possui agilidade sobre-humana, e a capacidade de teletransporte, invisibilidade em sombras profundas, e as mãos e os pés aderirem a parede.",
-        pesonagem:"/img/card-noturno.png"
+        pesonagen:"/img/card-noturno.png"
     
     },
     {
@@ -46,7 +46,7 @@ const pesonagens=[
         nome:"Tempestade",
         card:"/img/card-tempestade.jpg",
         poder:"é capaz de manipular o tempo, e alterar o clima, assim tendo influência sobre os elementos da natureza.",
-        pesonagem:"/img/card-tempestade.png"
+        pesonagen:"/img/card-tempestade.png"
     
     },
     {
@@ -54,7 +54,7 @@ const pesonagens=[
         nome:"Vampira",
         card:"/img/card-vampira.jpg",
         poder:"habilidade de absorver a vitalidade, memórias, habilidades e poderes de outros seres vivos através do contato com a pele.",
-        pesonagem:"/img/card-vampira.png"
+        pesonagen:"/img/card-vampira.png"
     
     },
     {
@@ -62,64 +62,52 @@ const pesonagens=[
         nome:'Wolverine',
         card:"/img/card-wolverine.jpg",
         poder:" Além de suas mortais garras de adamantium, capazes de cortar qualquer coisa, ele tem um poderoso fato de cura",
-        pesonagem:"/img/card-wolverine.png"
+        pesonagen:"/img/card-wolverine.png"
     
     },
     ] 
-    
-    
-    
-    
-    
-    
-    
-    
+
     
     function pesonagen(pesonagens){
         const divPesonagem= document.querySelector('.pessonagens' )
         const ul = document.createElement('ul');
         const div = document.createElement('div');
     
-        pesonagens.forEach((e,i)=>{ 
-            ul.innerHTML+=`<li><img src="${e.card}" id="${i}" ></li>`
-            
-        });   
+        pesonagens.forEach((e,i)=>{  ul.innerHTML+=`<li><img src="${e.card}" id="${i}" ></li>`});   
+        
         div.appendChild(ul);
+        
         divPesonagem.appendChild(div);
-    
     }
     
+   
+    
+    function pesonagemSelecionado(pesonagens){
+      const pesonagen =document.querySelector('.pesonagem-selecionado')
+      const descricaoPesonagen= document.querySelector('.pessonagens div ul ' )
+    
+        pesonagen.innerHTML=`
+        <div class="descricao-pesonagen">
+            <div>
+            <img src="${pesonagens[0].pesonagen}"> 
+            <h1>${pesonagens[3].nome}</h1>
+            </div>
+            <p>${pesonagens[3].poder}</p>
+         </div>
+       ` 
+     descricaoPesonagen.addEventListener('mouseover',(e)=>{
+        pesonagen.innerHTML=`     
+        <div class="descricao-pesonagen">
+           <div>
+           <img src="${pesonagens[e.target.id].pesonagen}"> 
+           <h1>${pesonagens[e.target.id].nome}</h1>
+           </div>
+           <p>${pesonagens[e.target.id].poder}</p>
+        </div>
+      `})
+    }
+    
+   
     pesonagen(pesonagens)
     
     pesonagemSelecionado(pesonagens)
-    
-    function pesonagemSelecionado(pesonagens){
-       const pesonagen =document.querySelector('.pesonagem-selecionado')
-    
-     
-       
-       pesonagen.innerHTML=`
-         
-    
-         <div class="descricao-pesonagen">
-            <div>
-            <img src="${pesonagens[1].pesonagen}"> 
-            <h1>${pesonagens[1].nome}</h1>
-            </div>
-            <p>${pesonagens[1].poder}</p>
-         </div>
-    
-       `
-    
-    }
-    
-    const img= document.querySelector('.pessonagens div ul li img' )
-    
-    console.log(img)
-    
-    img.addEventListener('mouseover',(e)=>{
-        e.forEach((e)=>{
-            console.log(e)
-        })
-    
-    })
